b27efde7571ec084317ec2d11c327fd57a25893d
Eddie Saunders <saunders.eddie@outlook.com>
2020-08-13 17:09:02 -0400

    Moved footer icons to header, confirmed links work, still need to style

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,21 +1,47 @@
 import React from "react";
-
+import EmailIcon from "../../assets/Images/EmailIcon.png";
+import FacebookLogo from "../../assets/Images/FacebookIcon.png";
+import GithubLogo from "../../assets/Images/gitHubLogo.png";
+import LinkedInLogo from "../../assets/Images/LinkedInIcon.png";
+import ResumeLogo from "../../assets/Images/Resume.png";
 const Header = () => {
   return (
     <>
-      <div className="sticky top-0 bg-gray-600">
+      <div className="sticky top-0 bg-white-600">
         <div className="flex mb-4">
           <div className="w-1/5">
-            <h1>email me</h1>
+            <a
+              href="https://github.com/SaundersEddie"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={GithubLogo} alt="Eddies Github" />
+            </a>
           </div>
           <div className="w-1/5">
-            <h1>Facebook</h1>
+            <a
+              href="facebook.com/EdwynSaunders"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={FacebookLogo} alt="Eddies Facebook" />
+            </a>
           </div>
+
           <div className="w-1/5">
-            <h1>GitHub</h1>
+            <a
+              href="https://www.linkedin.com/in/edwyn-saunders/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={LinkedInLogo} alt="Eddies LinkedIn" />
+            </a>
           </div>
+
           <div className="w-1/5">
-            <h1>LinkedIn</h1>
+            <a href="mailto:edwyn.saunders@outlook.com">
+              <img src={EmailIcon} alt="Email Eddie" />
+            </a>
           </div>
           <div className="w-1/5">
             <h1>Resume</h1>
