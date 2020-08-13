c0e40b910fd02b25acafe07a05ad474bd5a021f5
Eddie Saunders <saunders.eddie@outlook.com>
2020-08-13 16:38:41 -0400

    populated sticky header, removed sticky footer, to much excess

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+// import EmailIcon from "../../assets/Images/EmailIcon.png";
+// import FacebookIcon from "../../assets/Images/FacebookIcon.png";
+// import GithubLogo from "../../assets/Images/gitHubLogo.png";
+// import LinkedInIcon from "../../assets/Images/LinkedInIcon.png";
+// import ResumeLogo from "../../assets/Images/Resume.png";
 
 const Footer = () => {
-  return (
-    <>
-      <div className="sticky bottom-0 bg-gray-600">Static Footer</div>
-    </>
-  );
+  return <div className="sticky bottom-0 bg-white-600"></div>;
 };
 
 export default Footer;
--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -3,7 +3,25 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div className="sticky top-0 bg-gray-600">Static parent</div>
+      <div className="sticky top-0 bg-gray-600">
+        <div className="flex mb-4">
+          <div className="w-1/5">
+            <h1>email me</h1>
+          </div>
+          <div className="w-1/5">
+            <h1>Facebook</h1>
+          </div>
+          <div className="w-1/5">
+            <h1>GitHub</h1>
+          </div>
+          <div className="w-1/5">
+            <h1>LinkedIn</h1>
+          </div>
+          <div className="w-1/5">
+            <h1>Resume</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
