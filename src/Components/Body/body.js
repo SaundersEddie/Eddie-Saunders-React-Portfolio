3f802e8b4e4c35137974be66dea06e6532f25af6
Eddie Saunders <saunders.eddie@outlook.com>
2020-08-18 16:23:44 -0400

    Added in Roboto font, resolved issue with tailwindCSS sticky

--- src/Components/Body/body.js
+++ src/Components/Body/body.js
@@ -1,8 +1,14 @@
 import React from "react";
+import "./body.css";
 
 const Body = () => {
   return (
     <>
+      <h1 className="text-center myTitleText">
+        Please Allow Me to Introduce Myself...
+      </h1>
+      <h1 className="text-center myTitleText">Eddie Saunders</h1>
+
       <h1>All your base are belong to us</h1>
       <h1>All your base are belong to us</h1>
       <h1>All your base are belong to us</h1>
--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -3,52 +3,53 @@ import EmailIcon from "../../assets/Images/EmailIcon.png";
 import FacebookLogo from "../../assets/Images/FacebookIcon.png";
 import GithubLogo from "../../assets/Images/gitHubLogo.png";
 import LinkedInLogo from "../../assets/Images/LinkedInIcon.png";
-import ResumeLogo from "../../assets/Images/Resume.png";
+import ResumeLogo from "../../assets/Images/resumeLogo.png";
+import Pdf from "../../assets/documents/EddieSaundersResume.pdf";
+
+import "./header.css";
+
+// <div className="sticky top-0 bg-white-600 flex mb-4">
 const Header = () => {
   return (
-    <>
-      <div className="sticky top-0 bg-white-600">
-        <div className="flex mb-4">
-          <div className="w-1/5">
-            <a
-              href="https://github.com/SaundersEddie"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={GithubLogo} alt="Eddies Github" />
-            </a>
-          </div>
-          <div className="w-1/5">
-            <a
-              href="facebook.com/EdwynSaunders"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={FacebookLogo} alt="Eddies Facebook" />
-            </a>
-          </div>
-
-          <div className="w-1/5">
-            <a
-              href="https://www.linkedin.com/in/edwyn-saunders/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={LinkedInLogo} alt="Eddies LinkedIn" />
-            </a>
-          </div>
-
-          <div className="w-1/5">
-            <a href="mailto:edwyn.saunders@outlook.com">
-              <img src={EmailIcon} alt="Email Eddie" />
-            </a>
-          </div>
-          <div className="w-1/5">
-            <h1>Resume</h1>
-          </div>
-        </div>
+    <div className="myHeader sticky top-0 flex mb-4">
+      <div className="4-1/5 socialMediaIcon">
+        <a
+          href="https://github.com/SaundersEddie"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={GithubLogo} alt="Eddies Github" />
+        </a>
       </div>
-    </>
+      <div className="4-1/5 socialMediaIcon">
+        <a
+          href="https://facebook.com/EdwynSaunders"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={FacebookLogo} alt="Eddies Facebook" />
+        </a>
+      </div>
+      <div className="4-1/5 socialMediaIcon">
+        <a
+          href="https://www.linkedin.com/in/edwyn-saunders/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={LinkedInLogo} alt="Eddies LinkedIn" />
+        </a>
+      </div>
+      <div className="4-1/5 socialMediaIcon">
+        <a href="mailto:edwyn.saunders@outlook.com">
+          <img src={EmailIcon} alt="Email Eddie" />
+        </a>
+      </div>
+      <div className="4-1/5 socialMediaIcon">
+        <a href={Pdf} target="_blank" rel="noopener noreferrer">
+          <img src={ResumeLogo} alt="Eddie Resume" />
+        </a>
+      </div>
+    </div>
   );
 };
 
