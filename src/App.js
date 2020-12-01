dbde682c2d528937a8b7203926431100cb8ea12f
Eddie Saunders <saunders.eddie@outlook.com>
2020-12-01 15:18:58 -0500

    Code Cleanup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import Header from "./Components/Header/header";
 import Body from "./Components/Body/body";
 import Footer from "./Components/Footer/footer";
-
-import 'bootswatch/dist/lux/bootstrap.min.css'; // Added this :boom:
+import 'bootswatch/dist/lux/bootstrap.min.css';
 import './App.css';
 
 function App() {
