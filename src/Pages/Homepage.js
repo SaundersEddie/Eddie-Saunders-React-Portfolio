e6addaa3c75a15e449998d819d4d90275445515f
Eddie Saunders <saunders.eddie@outlook.com>
2021-03-15 18:27:19 -0400

    Added images and links to projects

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {Container, Carousel } from 'react-bootstrap';
 
 import DCMetro from '../assets/Images/DC_Metro.jpg';
+import JSCRUD from '../assets/Images/JS_OnlyCRUD.jpg'
+import MusicPage from '../assets/Images/Music_Page.jpg'
+import Weather from '../assets/Images/React_weather.jpg'
 
 export default function Homepage() {
 
@@ -12,7 +15,7 @@ export default function Homepage() {
             <Carousel>
                 <Carousel.Item interval={2500}>
                     <a 
-                        href="http://www.google.com"
+                        href="https://saunderseddie.github.io/dc-metro-status/"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
@@ -22,6 +25,46 @@ export default function Homepage() {
                     />
                     </a>
                 </Carousel.Item>
+
+                <Carousel.Item interval={2500}>
+                    <a 
+                        href="http://www.google.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                    <img className="d-block w-100"
+                        src={JSCRUD}
+                        alt="JS CRUD Page Link"
+                    />
+                    </a>
+                </Carousel.Item>
+
+                <Carousel.Item interval={2500}>
+                    <a 
+                        href="https://awesome-hypatia-cfdcf6.netlify.app/index.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                    <img className="d-block w-100"
+                        src={MusicPage}
+                        alt="DC Metro Logo"
+                    />
+                    </a>
+                </Carousel.Item>
+
+                <Carousel.Item interval={2500}>
+                    <a 
+                        href="https://exs-weather-react.herokuapp.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                    <img className="d-block w-100"
+                        src={Weather}
+                        alt="DC Metro Logo"
+                    />
+                    </a>
+                </Carousel.Item>
+
             </Carousel>
         </Container>
     )
