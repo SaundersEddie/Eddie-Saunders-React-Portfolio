a64320db700f8d4589cc23a2926d666a9ae3e051
Eddie Saunders <saunders.eddie@outlook.com>
2021-03-16 12:20:37 -0400

    Formatting andcolor schemes

--- src/Pages/Aboutpage.js
+++ src/Pages/Aboutpage.js
@@ -6,7 +6,7 @@ export default function Aboutpage() {
         <Container>
             <h1>Please Allow Me to Introduce Myself</h1>
             <hr></hr>
-            <p classname="testPadding">
+            <p className="eAbout">
                 I started coding way back in 1983, when the in thing was 6510A (6502)
                 Machine Code on a Commodore 64, after working on the Commodore 64 for
                 many years I turned my attention to the wonderful world of 16Bit and
@@ -15,7 +15,7 @@ export default function Aboutpage() {
                 problems of people not leaving an office for days that had no shower
                 facilties pushed me to move onto pastures new.
             </p>
-            <p>
+            <p className="eAbout">
                 In 1998 I moved to the USA and started my career fresh, working in the
                 IT field for UPS, although I also looked after a small workforce
                 managament group and done the odd bit of QA type work on proprietary
@@ -23,7 +23,7 @@ export default function Aboutpage() {
                 relocate to Cary, NC to work on automation and human emulation projects
                 for a company where I remained until 2019.
             </p>
-            <p>
+            <p className="eAbout">
                 When my contract ended, I decided to take a small break while
                 considering where to go next, cue January 2020 when I decided to go into
                 Full Stack Web Development, this was quite a leap considering the
