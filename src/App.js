8b82f67a1de7086d229fc27e913a0c361f4a725c
Eddie Saunders <saunders.eddie@outlook.com>
2021-03-16 08:07:08 -0400

    Added in cards deck to carousel, and background

--- src/App.js
+++ src/App.js
@@ -16,14 +16,14 @@ export default class App extends Component {
     return (
       <Router>
         <Container className="p-0" fluid={false}>
-          <Navbar className="border-bottom" bg="light" expand="lg" sticky="top">
+          <Navbar className="border-bottom" bg="transparent" expand="lg" sticky="top">
             <Navbar.Brand>Eddie Saunders</Navbar.Brand>
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
-                <Link className='nav-link' to='/'> Home </Link>
-                <Link className='nav-link' to='/about'> About </Link>
-                <Link className='nav-link' to='/contact'> Contact </Link>
+                <Link className='nav-link' style={{color:"#feda6a"}} to='/'>Home</Link>
+                <Link className='nav-link' style={{color:"#feda6a"}} to='/about'>About</Link>
+                <Link className='nav-link' style={{color:"#feda6a"}} to='/contact'>Contact</Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Container, Carousel } from 'react-bootstrap';
+import {Container, Carousel, CardDeck, Card } from 'react-bootstrap';
 
 import DCMetro from '../assets/Images/DC_Metro.jpg';
-import JSCRUD from '../assets/Images/JS_OnlyCRUD.jpg'
-import MusicPage from '../assets/Images/Music_Page.jpg'
-import Weather from '../assets/Images/React_weather.jpg'
+import JSCRUD from '../assets/Images/JS_OnlyCRUD.jpg';
+import MusicPage from '../assets/Images/Music_Page.jpg';
+import Weather from '../assets/Images/React_weather.jpg';
+import WDII from '../assets/Images/WDII.jpg';
 
 export default function Homepage() {
 
@@ -14,57 +15,125 @@ export default function Homepage() {
             <hr></hr>
             <Carousel>
                 <Carousel.Item interval={2500}>
-                    <a 
-                        href="https://saunderseddie.github.io/dc-metro-status/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                    <img className="d-block w-100"
-                        src={DCMetro}
-                        alt="DC Metro Logo"
-                    />
-                    </a>
+                    <CardDeck>
+                        <Card border="info">
+                            <Card.Img variant="top" src={DCMetro} />
+                            <Card.Body>
+                                <Card.Title>DC Metro Status</Card.Title>
+                                <Card.Text>Blah Blah Blah</Card.Text>
+                            </Card.Body>
+                            <Card.Footer className="text-muted">Last Updated Today</Card.Footer>
+                        </Card>
+
+                        <Card border="info">
+                            <Card.Img variant="top" src={JSCRUD} />
+                            <Card.Body>
+                                <Card.Title>JS CRUD App</Card.Title>
+                                <Card.Text>Blah Blah Blah</Card.Text>
+                            </Card.Body>
+                            <Card.Footer className="text-muted">Last Updated Today</Card.Footer>
+                        </Card>
+
+                        <Card border="info">
+                            <Card.Img variant="top" src={Weather} />
+                            <Card.Body>
+                                <Card.Title>My Weather Lookup Page</Card.Title>
+                                <Card.Text>Blah Blah Blah</Card.Text>
+                            </Card.Body>
+                            <Card.Footer className="text-muted">Last Updated Today</Card.Footer>
+                        </Card>
+                    </CardDeck>
                 </Carousel.Item>
 
                 <Carousel.Item interval={2500}>
-                    <a 
-                        href="http://www.google.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                    <img className="d-block w-100"
-                        src={JSCRUD}
-                        alt="JS CRUD Page Link"
-                    />
-                    </a>
+                    <CardDeck>
+                        <Card border="info">
+                            <Card.Img variant="top" src={MusicPage} />
+                            <Card.Body>
+                                <Card.Title>My Music Page</Card.Title>
+                                <Card.Text>Blah Blah Blah</Card.Text>
+                            </Card.Body>
+                            <Card.Footer className="text-muted">Last Updated Today</Card.Footer>
+                        </Card>
+
+                        <Card border="info">
+                            <Card.Img variant="top" src={WDII} />
+                            <Card.Body>
+                                <Card.Title>What Day Is It?</Card.Title>
+                                <Card.Text>Blah Blah Blah</Card.Text>
+                            </Card.Body>
+                            <Card.Footer className="text-muted">Last Updated Today</Card.Footer>
+                        </Card>
+
+                        <Card border="info">
+                            <Card.Img variant="top" src={Weather} />
+                            <Card.Body>
+                                <Card.Title>My Weather Lookup Page</Card.Title>
+                                <Card.Text>Blah Blah Blah</Card.Text>
+                            </Card.Body>
+                            <Card.Footer className="text-muted">Last Updated Today</Card.Footer>
+                        </Card>
+                    </CardDeck>
                 </Carousel.Item>
 
-                <Carousel.Item interval={2500}>
-                    <a 
-                        href="https://awesome-hypatia-cfdcf6.netlify.app/index.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                    <img className="d-block w-100"
-                        src={MusicPage}
-                        alt="DC Metro Logo"
-                    />
-                    </a>
-                </Carousel.Item>
 
-                <Carousel.Item interval={2500}>
-                    <a 
-                        href="https://exs-weather-react.herokuapp.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                    <img className="d-block w-100"
-                        src={Weather}
-                        alt="DC Metro Logo"
-                    />
-                    </a>
-                </Carousel.Item>
+
+
             </Carousel>
         </Container>
     )
 }
+
+
+
+// <Carousel.Item interval={2500}>
+// <a 
+//     href="https://saunderseddie.github.io/dc-metro-status/"
+//     target="_blank"
+//     rel="noopener noreferrer"
+// >
+// <img className="d-block w-100"
+//     src={DCMetro}
+//     alt="DC Metro Logo"
+// />
+// </a>
+// </Carousel.Item>
+
+// <Carousel.Item interval={2500}>
+// <a 
+//     href="http://www.google.com"
+//     target="_blank"
+//     rel="noopener noreferrer"
+// >
+// <img className="d-block w-100"
+//     src={JSCRUD}
+//     alt="JS CRUD Page Link"
+// />
+// </a>
+// </Carousel.Item>
+
+// <Carousel.Item interval={2500}>
+// <a 
+//     href="https://awesome-hypatia-cfdcf6.netlify.app/index.html"
+//     target="_blank"
+//     rel="noopener noreferrer"
+// >
+// <img className="d-block w-100"
+//     src={MusicPage}
+//     alt="DC Metro Logo"
+// />
+// </a>
+// </Carousel.Item>
+
+// <Carousel.Item interval={2500}>
+// <a 
+//     href="https://exs-weather-react.herokuapp.com/"
+//     target="_blank"
+//     rel="noopener noreferrer"
+// >
+// <img className="d-block w-100"
+//     src={Weather}
+//     alt="DC Metro Logo"
+// />
+// </a>
+// </Carousel.Item>
